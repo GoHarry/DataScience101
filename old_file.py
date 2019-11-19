06b77bcf8f3b8b8d5a384dc0cd51a85a83d769a8
Harkirat Singh <sonubedi@gmail.com>
2019-11-18 21:59:14 -0500

    second commit

--- old_file.py
+++ old_file.py
@@ -5,11 +5,14 @@ Spyder Editor
 This is a temporary script file.
 """
 def convert_to_str(arg):
+    '''Returns argument in string type.'''
     return str(arg)
 
 def check_dupes(list_1,list_2):
+    '''Returns duplicate values after comparing both lists.'''
     return [item for item in list_1 if item in list_2]
 
 def pancakes(list_3):
+    '''Returns list with first value as pancakes.'''
     list_3[0]="pancakes"
     return list_3
