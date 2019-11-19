ed0647f3ced8a5ae78588f1eef629ac349f4a787
Harkirat Singh <sonubedi@gmail.com>
2019-11-18 21:49:58 -0500

    first commit

---
+++ new_file.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 18 21:47:04 2019
+
+@author: hksingh
+"""
+
+import old_file as f
+
+print(f.convert_to_str(3))
+print(f.check_dupes([1,2,3],[4,3,2]))
+print(f.pancakes(['you','are','sweet']))
---
+++ old_file.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+def convert_to_str(arg):
+    return str(arg)
+
+def check_dupes(list_1,list_2):
+    return [item for item in list_1 if item in list_2]
+
+def pancakes(list_3):
+    list_3[0]="pancakes"
+    return list_3
